3c33fafe65c84387d07da5083535a675f66282c6
rama3na <dyavarivenkat2662@gmail.com>
2025-02-17 15:34:48 +0530

    Initial commit

--- src/components/userinfo/Navbar.js
+++ src/components/userinfo/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         
         {/* Toggle Button for Mobile */}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon text-white"></span>
+          <span className="navbar-toggler-icon  "></span>
         </button>
 
         {/* Collapsible Navbar */}
